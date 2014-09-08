9aa30820068ed46f351f8e28095d84f6689a420f
Steven Rombauts <steven@kotuha.be>
2014-09-08 20:06:53 +0200

    Implement translation of a nucleotide (DNA/RNA) sequence to a protein sequence

---
+++ model/entity/gene.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Gene entity
+ *
+ * @author  Steven Rombauts <https://github.com/stevenrombauts>
+ */
+class ComGenesModelEntityGene extends KModelEntityRow
+{
+    public function getProteinSequence()
+    {
+        $lines = explode("\n", $this->dna_sequence);
+
+        if(isset($lines[0]) && substr($lines[0], 0, 1) == '>') {
+            array_shift($lines);
+        }
+
+        if(!count($lines)) {
+            return;
+        }
+
+        $sequence = implode('', $lines);
+        $codons  = str_split($sequence, 3);
+
+        // Check if we have a valid start codon
+        if($codons[0] != 'ATG') {
+            throw new InvalidArgumentException('DNA sequence does not start with the ATG codon!');
+        }
+
+        // Check if we have a valid stop codon
+        if(!in_array(end($codons), array('TAG','TAA','TGA'))) {
+            throw new InvalidArgumentException('DNA sequence does not end with a valid stop codon! (TAG, TAA or TGA)');
+        }
+
+        $model = $this->getObject('com://stevenrombauts/genes.model.aminoacids');
+
+        $protein = '';
+
+        reset($codons);
+        foreach($codons as $codon) {
+            $protein .= $model->codon($codon)->getRow();
+        }
+
+        if(substr($protein, -1) == '*') {
+            $protein = substr($protein, 0, -1);
+        }
+
+        $protein_sequence = '>'.$this->title.'_protein'.PHP_EOL.$protein;
+
+        return $protein_sequence;
+    }
+}
